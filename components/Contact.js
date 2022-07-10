16f5135823bc899a1f492566dca60d21c5596c37
unknown <aventuras030@gmail.com>
2022-07-10 23:09:41 +0200

    typo in linkedin link fixed

--- components/Contact.js
+++ components/Contact.js
@@ -75,7 +75,7 @@ const Contact = () => {
             </a>
           </Link>
           <Link href="https://www.linkedin.com/in/lars-jensen-6829b3244/">
-            <a href="target_blank">
+            <a target="target_blank">
               <Image
                 src="/LinkedIn 2.png"
                 layout="fixed"
